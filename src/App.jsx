c19b2525ac76366569ee82b1ba5b970f14b0643b
annisingh-code <anni7621@gmail.com>
2026-03-23 00:14:57 +0530

    chore: setup react app structure

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Layout from "./components/Layout";
+import CardGrids from "./components/CardGrid";
+import Users from "./pages/Users";
+import SideBar from "./components/Sidebar";
+
+function App() {
+  // const dummyUsers = [
+  //   { id: 1, name: "Anish" },
+  //   { id: 2, name: "Sanya" },
+  //   { id: 3, name: "Vikram" },
+  //   { id: 4, name: "Riya" },
+  // ];
+
+  const [activePage, setActivePage] = useState("users");
+
+  // const renderPage = () => {
+  //   if (activePage == "users") return <Users />;
+  //   if (activePage == "post") return <Posts />;
+  //   if (activePage == "album") return <Albums />;
+  // };
+
+  return (
+    <Layout setActivePage={setActivePage}>
+      {activePage === "users" && <Users />}
+      {activePage === "posts" && <Posts />}
+      {activePage === "albums" && <Albums />}
+    </Layout>
+  );
+}
+
+export default App;
